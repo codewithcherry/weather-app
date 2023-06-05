b4d6e0d4376ae76079222db094fe021be23261ac
Prashanth Naroju <prashanth.naro@gmail.com>
2023-06-05 11:01:19 +0530

    install and initialize and setup tailwind css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className='bg-cyan-600 p-4'>
+      <h1 className='text-red-500 text-2xl'>weather app</h1>
     </div>
   );
 }
